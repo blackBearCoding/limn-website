84219d8504fff72aeb1db18923ecb7e928a30d8d
Delaney Hanson <delaney.hanson@techtonic.com>
2019-07-28 12:54:40 -0600

    responsiveness

---
+++ src/MiscComponents/CompanyItem.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import img1 from '../assets/zenpathz3.jpg'
+import img2 from '../assets/zenpathz4.jpg'
+import img3 from '../assets/zenpathz5.jpg'
+
+
+const useStyles = makeStyles({
+    card: {
+        maxWidth: 345,
+        minHeight: 500,
+    },
+    media: {
+        height: 200,
+    },
+    actionArea: {
+        '&:hover': {
+            pointerEvents: 'none'
+        }
+    }
+});
+
+export default function CompanyItem() {
+    const classes = useStyles();
+
+    return (
+    <div className="card-holder">
+            <Card className={classes.card} style={{ backgroundColor: '#f06449', color: '#fefcfd' }}>
+            <CardActionArea className={classes.actionArea}>
+                <CardMedia
+                    className={classes.media}
+                    image={img2}
+                    title="Contemplative Reptile"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        What we do
+          </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p" style={{ color: '#fefcfd' }}>
+                        With over twenty  years experience building multimedia software including games, simulations, websites,  and interactive worlds, our current projects are being built by developers, artists, producers, musicians, testers, and marketing individuals located primarily in the Colorado, USA area.
+          </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                    <Button size="small" color="primary" style={{ color: '#fefcfd' }}>
+                    Learn More
+        </Button>
+            </CardActions>
+        </Card>
+            <Card className={classes.card} style={{ backgroundColor: '#2d3142', color: '#fefcfd'  }}>
+                <CardActionArea className={classes.actionArea} >
+                    <CardMedia
+                        className={classes.media}
+                        image={img1}
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Why we do it
+          </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p" style={{ color: '#fefcfd' }}>
+                            The majority of titles coming out today are rushed to market in part due to over-run schedules and budgets.  As a result, these titles are often missing critical features that can make a big difference.  We have taken a different approach with a process and tools that allow more polish and testing phases earlier in the development methodology.
+          </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary" style={{ color: '#fefcfd' }}>
+                        Learn More
+        </Button>
+                </CardActions>
+            </Card>
+            <Card className={classes.card} style={{ backgroundColor: '#05668d', color: '#fefcfd' }}>
+                <CardActionArea className={classes.actionArea}>
+                    <CardMedia
+                        className={classes.media}
+                        image={img3}
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            How we do it
+          </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p" style={{ color: '#fefcfd' }}>
+                            Crash against wall but walk away like nothing happened nyan fluffness ahh cucumber! yet chirp at birds but Gate keepers of hell cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats firefighters' slippers. yet chirp at birds but Gate keepers of hell cat gets stuck in tree firefighters try to get cat down firefighters get stuck.
+          </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary" style={{ color: '#fefcfd' }}>
+                        Learn More
+        </Button>
+                </CardActions>
+            </Card>
+    </div>
+    );
+}
--- src/Pages/OurTeamPage.js
+++ src/Pages/OurTeamPage.js
@@ -3,16 +3,20 @@ import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import jet from '../assets/jet.jpg'
 import scott from '../assets/scott.jpg'
+import './OurTeamPage.css'
 
 export default class OurTeamPage extends Component {
     render() {
         return (
             <>
-               <Grid container>
-                   <Grid item span={4}>
-                       <img src={jet} alt="Jet Ternlund" style={{width: '30em', margin: '1em'}}></img>
+               <Grid container
+                    justify="center"
+                    className="container1"
+               >
+                   <Grid item span={4} className="grid">
+                       <img src={jet} alt="Jet Ternlund" style={{width: '30em', margin: '1em'}} className="img"></img>
                    </Grid>
-                   <Grid item span={4} >
+                    <Grid item span={4} className="grid">
                         <Typography variant="h2" style={{marginTop: '0.5em', marginLeft: '0.25em'}}>
                             Jet Ternlund
                        </Typography>
@@ -22,9 +26,10 @@ export default class OurTeamPage extends Component {
                    </Grid>
                </Grid>
                <Grid container
-               justify="flex-end"
+               justify="center"
+                    className="container2"
                >
-                    <Grid item span={4}>
+                    <Grid item span={4} className="grid">
                         <Typography variant="h2" style={{ marginTop: '0.5em', marginLeft: '0.25em' }}>
                             Scott Emberty
                        </Typography>
@@ -32,8 +37,8 @@ export default class OurTeamPage extends Component {
                             Put toy mouse in food bowl run out of litter box at full speed if it fits i sits demand to be let outside at once, and expect owner to wait for me as i think about it yet proudly present butt to human so groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked. Annoy the old grumpy cat, start a fight and then retreat to wash when i lose i will ruin the couch with my claws, meeeeouw when in doubt, wash or chase ball of string chase laser let me in let me out let me in let me out let me in let me out who broke this door anyway.
                        </Typography>
                     </Grid>
-                    <Grid item span={4}>
-                        <img src={scott} alt="Scott Emberty" style={{ width: '30em', margin: '1em' }}></img>
+                    <Grid item span={4} className="grid">
+                        <img src={scott} alt="Scott Emberty" style={{ width: '30em', margin: '1em' }} className="img"></img>
                     </Grid>
                </Grid> 
             </>
